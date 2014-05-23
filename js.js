f4cf62b87954610810da4d23f79d02cd827820e9
Guilherme Souza <sitegui@sitegui.com.br>
2014-05-23 09:58:23 -0300

    New algorithm for curve simplifying

--- js.js
+++ js.js
@@ -20,38 +20,68 @@ addEventListener("load", function () {
 		requestAnimationFrame(drawLoop)
 	}
 	
-	// Process clicks
-	var pressing = false
-	var process = function (event, canBegin) {
+	var touch2point = function (touch) {
 		var box = bigCanvas.getBoundingClientRect()
-		var x = event ? event.clientX - box.left : 0
-		var y = event ? event.clientY - box.top : 0
-		x -= 118
-		y -= 28
+		var x = touch.clientX - box.left - 118
+		var y = touch.clientY - box.top - 28
 		var p = new Point(x, y)
-		var isIn = x >= 0 && x < 568 && y >= 0 && y < 320
-		
-		if (pressing) {
-			if (isIn) {
-				touchMoved(p)
-			} else {
-				pressing = false
-				touchEnded()
-			}
-		} else if (isIn && canBegin) {
-			pressing = true
+		p.isIn = x >= 0 && x < 568 && y >= 0 && y < 320
+		return p
+	}
+	
+	var touching = false
+	var processBegin = function (event, touch) {
+		var p = touch2point(touch)
+		if (touching) {
+			touching = false
+			touchEnded(p)
+		}
+		if (p.isIn) {
+			event.preventDefault()
+			touching = true
 			touchBegan(p)
 		}
 	}
 	
+	var processMove = function (event, touch) {
+		var p = touch2point(touch)
+		if (touching) {
+			if (p.isIn) {
+				event.preventDefault()
+				touchMoved(p)
+			} else {
+				touching = false
+				touchEnded(p)
+			}
+		}
+	}
+	
+	var processEnd = function (event, touch) {
+		var p = touch2point(touch)
+		if (touching) {
+			touching = false
+			touchEnded(p)
+		}
+	}
+	
+	// Touch events
 	bigCanvas.onmousedown = function (event) {
-		process(event, true)
+		processBegin(event, event)
 	}
 	bigCanvas.onmousemove = function (event) {
-		process(event, false)
+		processMove(event, event)
 	}
-	bigCanvas.onmouseup = function () {
-		process(null)
+	bigCanvas.onmouseup = function (event) {
+		processEnd(event, event)
+	}
+	bigCanvas.ontouchstart = function (event) {
+		processBegin(event, event.changedTouches[0])
+	}
+	bigCanvas.ontouchmove = function (event) {
+		processMove(event, event.changedTouches[0])
+	}
+	bigCanvas.ontouchend = function () {
+		processEnd(event, event.changedTouches[0])
 	}
 	
 	var drawLoop = function (time) {
@@ -63,6 +93,10 @@ addEventListener("load", function () {
 	}
 })
 
+function rad2degree(rad) {
+	return (rad/Math.PI*180).toFixed(1)
+}
+
 var points = []
 var allPoints = []
 var anchor = null
@@ -73,30 +107,53 @@ function touchBegan(p) {
 	anchor = null
 }
 
-function rad2degree(rad) {
-	return (rad/Math.PI*180).toFixed(1)
-}
-
 function touchMoved(p) {
 	var last = points[points.length-1]
-	var angle1, angle2, dangle
+	var angle1, angle2, dangle, newAngle, dist1, dist2, dist
+	
+	// Store the raw point
 	allPoints.push(p)
-	if (p.getSquareDistance(last) > 400) {
+	
+	if (p.getSquareDistance(last) > 50*50) {
+		// Respect a minimum distance
+		
 		if (anchor) {
 			angle1 = anchor.getAngleTo(last)
 			angle2 = last.getAngleTo(p)
 			dangle = Math.abs((angle1-angle2)%(2*Math.PI))
 			dangle = dangle>Math.PI ? 2*Math.PI-dangle : dangle
 			if (dangle < 15*Math.PI/180) {
+				// Simplify
+				
+				// Remove last point
 				points.pop()
+				
+				// New angle
+				dist1 = Math.sqrt(anchor.getSquareDistance(last))
+				dist2 = Math.sqrt(last.getSquareDistance(p))
+				newAngle = (dist1*angle1+dist2*angle2)/(dist1+dist2)
+				
+				// Distance from point to point'
+				dist = (p.x-anchor.x)*Math.sin(newAngle)-(p.y-anchor.y)*Math.cos(newAngle)
+				dist = Math.abs(dist)
+				
+				p.x += dist*Math.cos(newAngle+Math.PI/2)
+				p.y += dist*Math.sin(newAngle+Math.PI/2)
+			} else {
+				// Don't simplify
+				
+				
 			}
 		}
+		
 		anchor = last
 		points.push(p)
 	}
 }
 
-function touchEnded() {
+function touchEnded(p) {
+	allPoints.push(p)
+	points.push(p)
 	console.log("Selected %d of %d acquired points (%d%%)", points.length, allPoints.length, Math.round(100*points.length/allPoints.length))
 }
 
@@ -129,4 +186,6 @@ function drawRect(canvas, context, time) {
 			context.moveTo(allPoints[i].x, allPoints[i].y)
 	}
 	context.stroke()
+	
+	context.strokeRect((568-320)/2, 0, 320, 320)
 }
